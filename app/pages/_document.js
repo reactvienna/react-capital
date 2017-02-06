fec36e1c9451aa6bad728bf74c4e69a42ca49c2e
Max <contact@mstoiber.com>
2017-02-06 17:17:26 +0100

    Initial setup

---
+++ app/pages/_document.js
@@ -0,0 +1,27 @@
+/**
+ * Override the Next.js default document to provide styled-components SSR support
+ */
+import Document, { Head, Main, NextScript } from 'next/document'
+import { styleSheet } from 'styled-components'
+
+export default class CustomDocument extends Document {
+	static async getInitialProps ({ renderPage }) {
+		const page = renderPage()
+		const style = styleSheet.getCSS()
+		return { ...page, style }
+	}
+
+	render () {
+		return (
+			<html>
+				<Head>
+					<style dangerouslySetInnerHTML={{ __html: this.props.style }} />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</html>
+		)
+	}
+}
---
+++ app/pages/index.js
@@ -0,0 +1,5 @@
+import Wrapper from '../components/Wrapper'
+
+export default () => (
+	<Wrapper>Homepage</Wrapper>
+)
